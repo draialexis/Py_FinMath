41976bbd5604ac0edf688bf63913cc57ddb0857e
Alexis DRAI <drai.alexis@gmail.com>
2022-02-19 15:14:27 +0100

    Make init vars global

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 # 1)
 
+global duree
+global investissement
+global revente
+global benefices
+
+
 def init_var_glob():
     # la durée considérée pour le projet, en années
+    global duree
     duree = 7
     # l'investissement, en début de projet
+    global investissement
     investissement = 10400
     # le prix de revente du matériel, en fin de projet
+    global revente
     revente = 1040
     # les bénéfices réalisés en fin d'année
+    global benefices
     benefices = [6100, 6400, 4300, 200, 4900, 4400, 1500]
 
+# 2)
