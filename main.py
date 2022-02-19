6ec35d7af69cd27411d29b148f5706dec29ce7c9
Alexis DRAI <drai.alexis@gmail.com>
2022-02-19 14:59:29 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# 1)
+
+def init_var_glob():
+    # la durée considérée pour le projet, en années
+    duree = 7
+    # l'investissement, en début de projet
+    investissement = 10400
+    # le prix de revente du matériel, en fin de projet
+    revente = 1040
+    # les bénéfices réalisés en fin d'année
+    benefices = [6100, 6400, 4300, 200, 4900, 4400, 1500]
+
