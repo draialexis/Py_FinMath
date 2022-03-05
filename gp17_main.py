22de137910382c393e93e605c5940b95b36667ed
Alexis DRAI <drai.alexis@gmail.com>
2022-03-05 08:10:09 +0100

    Elyes -- Prettify

--- gp17_main.py
+++ gp17_main.py
@@ -1,36 +1,44 @@
+
+
 epsilon = 0.0001
 t_min = 0.0
 t_max = 1.0
 nom_fichier = "gp17_data.txt"
 
-# 1)
 
-# la durée considérée pour le projet, en années
+#   1)
+
+# La durée considérée pour le projet, en années
 global DUREE
-# l'investissement, en début de projet
+# L'investissement, en début de projet
 global INIT_FLUX
-# les bénéfices réalisés en fin d'année
+# Les bénéfices réalisés en fin d'année
 global BENEFICES
-# le prix de revente du matériel, en fin de projet
+# Le prix de revente du matériel, en fin de projet
 global REVENTE
 REVENTE = 0
 
 
-# 2)
 
 
-# on suppose que les fichers xlsx suivent tous le même format
+#   2)
+
+# On suppose que les fichers xlsx suivent tous le même format
 def lecture_donnees(in_donnees_xlsx):
+
     global DUREE
     global INIT_FLUX
     global BENEFICES
     global REVENTE
 
+
     try:  # si le fichier existe, on peut exploiter les données
         with open(in_donnees_xlsx) as f:
             for line_num, line in enumerate(f):
+
                 if line_num == 0:
                     DUREE = int(line.strip())
+
                 if line_num == 2:
                     data = line.strip().split(";")
                     INIT_FLUX = int(data.pop(0))  # on retire le premier element et on copie sa valeur dans init_flux
@@ -39,45 +47,58 @@ def lecture_donnees(in_donnees_xlsx):
 
     # si le fichier est introuvable, on utilise en dur les données correspondant au groupe 17 par défaut
 
+
     except FileNotFoundError:
-        print("\n[WARNING] le fichier "
+
+        print("\n [WARNING] le fichier "
               + in_donnees_xlsx +
               " est introuvable, donc init_dicho() utilise en dur les donnees correspondant au groupe 17 par defaut.")
+
         DUREE = 7
         INIT_FLUX = -10400
         BENEFICES = [6100, 6400, 4300, 2000, 4900, 4400, 1500]
         REVENTE = 1040
 
 
-# 3)
 
-# on applique la formule du calcul de la VAN
+
+#   3)
+
+# On applique la formule du calcul de la VAN
 def calcul_van(in_taux):
+
     if in_taux < 0:
-        exit("calcul_VAN(): taux < 0 INVALIDE ( in_taux =" + str(in_taux) + ")")
+        exit(" calcul_VAN(): taux < 0 INVALIDE ( in_taux =" + str(in_taux) + ")")
 
     van = INIT_FLUX
+
     for i, benefice in enumerate(BENEFICES):
         van = van + (benefice / pow(1 + in_taux, i + 1))  # attention, i est 0-indexé...
 
     van = van + (REVENTE / pow(1 + in_taux, DUREE))
+
     return van
 
 
-# 4)
 
-# procedure de validation des données du projet
+
+#   4)
+
+# Procedure de validation des données du projet
 def init_dicho(in_t_min, in_t_max):
+
     profits = 0.0
 
     for flux in BENEFICES:
         profits += flux
 
-    # on vérifie que :
+
+    # On vérifie que :
     # le projet est profitable en théorie
     # le flux initial est bien négatif
     # le taux minimum initial de dicho n'est pas négatif (VAN(t) est définie sur R+, pas sur tout R)
     # le taux minimum initial est bien strictement inférieur au taux maximum initial
+
     if profits + INIT_FLUX < 0 \
             or INIT_FLUX > 0 \
             or in_t_min < 0 \
@@ -85,44 +106,52 @@ def init_dicho(in_t_min, in_t_max):
         exit("init_dicho(): la question du TRI ne s'applique pas à ces données")
 
 
-# 5)
 
-# suivi exact de la procédure à l'algo 5
-# diviser pour regner, convergence rapide vers l'objectif...
+
+#   5)
+
+# Suivi exact de la procédure à l'algo 5
+# Diviser pour regner, convergence rapide vers l'objectif...
 def dichotomie(in_t_min, in_t_max, in_epsilon):
+
     init_dicho(in_t_min, in_t_max)  # validation grace à init_dicho
 
     t_ri = 0.0
     arret = False
+
     while not arret:
         t_c = (in_t_max + in_t_min) / 2
         van_c = calcul_van(t_c)
+
         if van_c >= in_epsilon:
             in_t_min = t_c
+
         elif van_c <= - in_epsilon:
             in_t_max = t_c
+
         else:
             t_ri = t_c
             arret = True
 
+
     return t_ri
 
 
-# 6)
+#   6)
 
 def affichage_resultat(in_t_min, in_t_max, in_epsilon, in_t_ri):
-    print("\nnombre de periodes (n) : " + str(DUREE))
-    print("premier flux (I) : " + str(INIT_FLUX))
-    print("benefices (B_i) : " + str(BENEFICES))
-    print("valeur de revente de l'equipement (V) : " + str(REVENTE))
-    print("\n[INFO] les bornes et l'epsilon peuvent etre modifies en dur, aux lignes 3, 4, et 5")
+    print("\n  Nombre de periodes (n) : " + str(DUREE))
+    print("  Premier flux (I) : " + str(INIT_FLUX))
+    print("  Benefices (B_i) : " + str(BENEFICES))
+    print("  Valeur de revente de l'equipement (V) : " + str(REVENTE))
+    print("\n [INFO] les bornes et l'epsilon peuvent etre modifies en dur, aux lignes 3, 4, et 5")
 
-    print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-    print("taux - borne inferieure : " + str(in_t_min) + " ; borne superieure : " + str(in_t_max))
-    print("epsilon : " + str(in_epsilon))
-    print("taux de rendement interne du projet : " + str(in_t_ri))
-    print("soit " + str(round((in_t_ri * 100), 2)) + "%")
-    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("  Taux : borne inferieure = ( " + str(in_t_min) + " ) ; borne superieure = ( " + str(in_t_max) + " )")
+    print("  Epsilon : " + str(in_epsilon))
+    print("  Taux de rendement interne du projet : " + str(in_t_ri))
+    print("  Soit " + str(round((in_t_ri * 100), 2)) + "%")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
 
 # TEST
